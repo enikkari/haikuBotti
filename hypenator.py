a312bde865cab943eef86ead186c47f0ca9c8cbf
Eeva Nikkari <enik@91-159-206-197.elisa-laajakaista.fi>
2017-09-26 20:47:34 +0300

    markovchain and hypenation for finnish words

---
+++ hypenator.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+# Hypenation for finnish words
+# makes mistakes in compound words
+# Rules were derived from the finnish hypenation wikipedia article :)
+
+vowel = {'a', 'e', 'i', 'o', 'u', 'y', 'å'.decode('utf-8'), 'ä'.decode('utf-8'), 'ö'.decode('utf-8')}
+
+diphthong = {'ai', 'ei', 'oi', 'ui', 'yi', 'äi'.decode('utf-8'), 'öi'.decode('utf-8'), 'au', 'eu', 'iu', 'ou',
+             'äy'.decode('utf-8'), 'öy'.decode('utf-8'), 'iy', 'ey', 'ie', 'uo', 'yö'.decode('utf-8')}
+
+punct = {'.', ',', ':', ';', '?', '!', '/', '\'', '/"', '-', '(', ')', '[', ']', '{', '}'}
+
+
+class Hypenator:
+    def __init__(self, language):
+        if (language == 'finnish'):
+            self.hype = self.fin_hype
+        else:
+            print 'not supported'
+
+    def fin_hype(self, word):
+        w = list(word.lower())
+        hype = []
+        syl = [w[0]]
+        for i in range(1, len(w)):
+            prw = syl[-1]
+            nw = w[i]
+            if nw in punct:
+                hype.append("".join(syl))
+            # hyphenate between two vowels if they don't form a long vowel or diftong
+            elif prw in vowel and nw in vowel and prw != nw and (prw + nw) not in diphthong:
+                hype.append("".join(syl))
+                syl = [nw]
+            # hyphenate before consonant that is followed by a vowel
+            elif prw not in vowel and nw in vowel and i > 2:
+                if len(syl) > 1:
+                    hype.append("".join(syl[0:-1]))
+                syl = [prw, nw]
+            else:
+                syl.append(nw)
+        hype.append("".join(syl))
+        return hype
+
+    def count_syl(self, t_text):
+        return sum([len(self.hype(w)) for w in t_text])
---
+++ markovchain.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+import random
+import re
+import string
+import hypenator as hp
+
+# TODO: implement markov chain as a ngram model in ARPA format with back-off
+# current method is ineffective with large corpora
+
+def get_words(text):
+    text = re.sub("-", " ", text)
+    exclude = string.punctuation + '0123456789' +"\.-"+ "\"" +"”“”–" #string.punctuation
+    exclude = exclude.decode("utf-8")
+    regex = re.compile('[%s]' % re.escape(exclude))
+    text = text.lower()
+    text = regex.sub(' ', text)
+    tokens = text.split()
+    return tokens
+
+class MarkovChain:
+    def __init__(self, n, language='finnish'):
+        self.n = n
+        # self.words = list(set(t_content))
+        # self.uwords = dict()#dict.fromkeys([w for w in self.words])
+        self.map = {}
+        self.map[""] = [""]
+        # map.learn(t_content, self.n)
+        self.words = []
+        self.hype = hp.Hypenator(language)
+
+    def learn(self, content):
+        if len(content)>0:
+            t_content = get_words(content)
+            map = self.map
+            self.words = list(set(self.words + t_content))
+            n= self.n
+            for i in range(0, len(t_content)):
+                word = t_content[i]  # huom
+                for j in range(0, n + 1):
+                    key = " ".join(t_content[i + 1: i + 1 + j])
+                    if key in map:
+                        map[key].append(word)
+                    else:
+                        map[key] = [word]
+        #return map
+
+    def text(self, nwords, last_word=""):
+        state = last_word
+        if state == "":
+            state = random.choice(self.words)
+        text = [state]
+        for i in range(1, nwords):
+            if (state not in self.map.keys()):
+                state = random.choice(self.map.keys())  # huom
+            word = random.choice(self.map[state])
+            text = [word] + text
+            state = " ".join(text[0:self.n])
+        return text
+
+    # check a long sequence of tokenized words for n ones that matches the Kalevala restrictions
+    def kalevala(self, nlines=4, last_word=""):
+        tn = 100
+        lines = []
+        iterations = 0
+        while len(lines) < nlines and iterations < 4:
+            text = self.text(tn, last_word)
+            a = 0
+            b = 1
+            while b < tn and a < tn:
+                syl = self.hype.count_syl(text[a:b])
+                if syl < 8:
+                    b = b + 1
+                elif syl > 8:
+                    a = a + 1
+                else:
+                    """8 syllables in sequence
+					 check if at least two of the words have same first letter """
+                    first_letters = [list(w)[0] for w in text[a:b]]
+                    set_first_letters = set(first_letters)
+                    uniq = len(set_first_letters)
+                    words = len(first_letters)
+                    if uniq <= 2 and words > uniq:
+                        lines.append(" ".join(text[a:b]))
+                        a = b
+                        b = b + 1
+                        if len(lines) == nlines:
+                            break
+                    else:
+                        a = a + 1
+            iterations = iterations + 1
+        if iterations == 4 and len(lines) != 4:
+            print "not enough lines found"
+        return "\n".join(lines)
+
+    def haiku(self, last_word=""):
+        # 5-7-5
+        tn = 15
+        poem = ['none found']
+        iterations = 0
+        while iterations < 10:
+            text = self.text(tn, last_word)
+            a = 0
+            b = 1
+            while b < tn and a < tn:
+                syl = self.hype.count_syl(text[a:b])
+                if syl < 17:
+                    b = b + 1
+                elif syl > 17:
+                    a = a + 1
+                else:
+                    c = a + 1
+                    d = b - 1
+                    while self.hype.count_syl(text[a:c]) < 5:
+                        c = c + 1
+                    while self.hype.count_syl(text[d:b]) < 5:
+                        d = d - 1
+                    if self.hype.count_syl(text[a:c]) == 5 and self.hype.count_syl(
+                            text[c:d]) == 7 and self.hype.count_syl(text[d:b]) == 5:
+                        poem = [text[a:c], text[c:d], text[d:b]]
+                        poem = [" ".join(l) for l in poem]
+                        break
+                    else:
+                        a = a + 1
+            iterations = iterations + 1
+        return "\n".join(poem)
